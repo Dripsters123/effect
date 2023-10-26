9020f3a568aa99e4c70598b42a9f16855187e6fd
Your Name <you@example.com>
2023-10-26 09:52:35 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import ToDo from "./ToDo";
+function App() {
+  const todo = [{
+    userId: 1,
+    id:1,
+    title:"delectus aut autem",
+    completed:false,
+  }];
+
+  return (
+    <>
+    <ToDo/>
+    </>
+   
+  );
+}
+
+export default App;
---
+++ src/ToDo.js
@@ -0,0 +1,13 @@
+import {useState} from "react";
+function ToDo(props){
+    
+return (
+    <>
+<p>UserId: 1</p>
+<p>Id:1</p>
+<p>delectus aut autem</p>
+<input type="checkbox"></input>
+    </>
+)
+}
+export default ToDo;
