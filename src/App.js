771f027ecb8ad97fa881e41162d76d9c041620b7
Your Name <you@example.com>
2023-10-31 09:53:05 +0200

    gaidit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import { useEffect, useState } from "react";
 import ToDo from "./ToDo";
 
 function App() {
-  const toDo = {
-    userId: 1,
-    id:1,
-    title:"delectus aut autem",
-    completed:false,
-  };
+  const [toDo, setToDo] = useState({});
+  
+ 
+  useEffect(() =>{
+    async function getToDo(){
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+      const data = await  response.json();
+      setToDo(data);
+    };
+    getToDo();
+  }, []);
+
+
+  // const toDo = {
+  //   userId: 1,
+  //   id:1,
+  //   title:"delectus aut autem",
+  //   completed:false,
+  // };
 
   return (
     <>
+    
     <ToDo {...toDo}/>
     </>
    
